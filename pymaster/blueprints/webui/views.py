f4e307bb86c214f8837f073afeee90d9cd46343b
Fábio Vitor <48036134+FVitor7@users.noreply.github.com>
2021-09-16 00:23:08 -0300

    Update views.py

--- pymaster/blueprints/webui/views.py
+++ pymaster/blueprints/webui/views.py
@@ -3,12 +3,12 @@ from pymaster.models import Players
 from flask_simplelogin import login_required
 
 
-@login_required
+#@login_required
 def index():
     return render_template("index.html")
     #return render_template("index.html")
 
-@login_required
+#@login_required
 def player(player_id):
     player = Players.query.filter_by(id=Players.PlayerID).first() or abort(
         404, "Jogador não encontrado"
